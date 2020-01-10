d6ece566b564520894ede7b26ba926f0d4181f88
Amit <lordamit@gmail.com>
2020-01-09 23:47:01 -0500

    added dialogfragmentshow from google drive

---
+++ DialogFragmentShow/app/src/main/java/edu/wm/cs/semeru/exampleapp/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.wm.cs.semeru.exampleapp;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DialogFragment example = new ExampleDialogFragment();
+        example.show(getFragmentManager(), "ExampleDialog");
+    }
+
+    public static class ExampleDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            String dataLeAk = java.util.Calendar.getInstance().getTimeZone().getDisplayName();
+            android.util.Log.d("leak-1", dataLeAk);
+            return null;
+        }
+    }
+}
